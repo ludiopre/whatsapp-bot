f8f91f479a296b6aed88aff3a0340db9c14f13ea
ludiopre <ludiopre@gmail.com>
2025-04-12 18:18:38 -0500

    Add files via upload
    Subo el bot funcional versión inicial

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const comerciantes = require('./routes/comerciantes');
+const productos = require('./routes/productos');
+const webhook = require('./routes/webhook');
+const compras = require('./routes/compras');
+const alertas = require('./routes/alertas');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.use('/comerciantes', comerciantes);
+app.use('/productos', productos);
+app.use('/webhook', webhook);
+app.use('/comprar', compras);
+app.use('/alertas', alertas);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`AutoBot API activa en puerto ${PORT}`));
