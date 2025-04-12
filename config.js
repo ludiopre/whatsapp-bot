944ac0c30ed8785f59857dc6263ef0afdd22d46e
ludiopre <ludiopre@gmail.com>
2025-04-12 18:48:08 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+exports.productos = {
+  papel: `🧻 Papel higiénico institucional\nHoja doble, x12 rollos de 300m\n💲 $48.000\nResponde "comprar" para hacer tu pedido o "menu" para volver.`,
+  jabon: `🧼 Jabón lavaloza institucional\nPresentación: galón o caneca\n💲 Desde $30.000\nResponde "comprar" para hacer tu pedido o "menu" para volver.`,
+  esponja: `🧽 Esponjillas doble capa\nPaquete x12 unidades\n💲 $15.000\nResponde "comprar" para hacer tu pedido o "menu" para volver.`,
+};
