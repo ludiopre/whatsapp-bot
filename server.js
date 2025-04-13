a630dda25d9e65e07d6344a07f6484b9e565c453
ludiopre <ludiopre@gmail.com>
2025-04-12 19:04:18 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const productos = require('./routes/productos');
 const webhook = require('./routes/webhook');
 const compras = require('./routes/compras');
 const alertas = require('./routes/alertas');
